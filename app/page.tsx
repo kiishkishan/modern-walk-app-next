4ad95bf20b208e258f13614aa9f4cda71661cb3e
theeps <theepi27@gmail.com>
2024-05-05 17:10:19 +0530

    loading component added

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ const HomePageContent: React.FC = () => {
     // Simulating API call delay
     setTimeout(() => {
       setLoading(false)
-    }, 1000)
+    }, 1500)
   }, [])
 
   return (
--- components/loading.tsx
+++ components/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Loading: React.FC = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-200">
       <p className="text-lg font-semibold">Loading...</p>
     </div>
   )
