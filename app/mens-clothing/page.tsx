65240dda3de01a41f4302afe9108bad050ee86c8
kiishkishan <kishanth001@gmail.com>
2024-05-18 11:58:35 +0530

    fixed routing and simplified code

---
+++ app/mens-clothing/page.tsx
@@ -0,0 +1,15 @@
+// app/mens-clothing/page.tsx
+
+import React from 'react'
+import { fetchMensClothing } from '@/utils/api'
+import CategoryPage from '@/components/categoryPage'
+import { ClothingItemTypes } from '@/constants/types'
+
+const MensClothingPage = async () => {
+  // Fetch data for men's clothing items
+  const mensClothingData: ClothingItemTypes[] = await fetchMensClothing()
+
+  return <CategoryPage title="Men's Clothing" categoryData={mensClothingData} />
+}
+
+export default MensClothingPage
